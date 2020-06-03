82aa31a63d02ee07224e945ed559cbe57fa22e6c
Ubuntu <ubuntu@ip-172-31-26-232.us-east-2.compute.internal>
2020-06-03 09:33:15 +0000

    Add edit/update actions and flash messages.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+     include ArticlesHelper
 def index
   @articles = Article.all
 end
@@ -11,11 +12,26 @@ def new
 end
 
 def create
-  @article = Article.new( title: params[:article][:title],
-   body: params[:article][:body]
-  )
+  @article = Article.new(article_params)
   @article.save
   redirect_to article_path(@article)
+end
+
+def edit
+  @article = Article.find(params[:id])
+end
+
+def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+    flash.notice = "Article '#{@article.title}' Updated!"
+    redirect_to article_path(@article)
+end
+
+def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
 end
 
 end
